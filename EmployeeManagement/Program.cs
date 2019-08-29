63401d61f6874a55b0faffdbeb4984e2a47ccef2
a4aditya4 <a4aditya4@gmail.com>
2019-08-29 23:24:28 +0530

    initial code

---
+++ EmployeeManagement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace EmployeeManagement
+{
+    //usually console application has program class and Main() method 
+    //ASP .Net core app initially starts as console app and main() enrty point for the application.this main method configures asp.net core and starts it and at that point it becomes an ASP.Net core web application
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        //CreateDefaultBuilder(args) is static method that create WebHost with preconfigured defaults.this method maintains the default order of configuartion sources
+        //some of the tasks that CreateDefaultBuilder(args) performs
+        //1)setting up the WebServer
+        //2)Loading the host and application configuration from various configuration sources and
+        //3)configuring logging
+
+
+        //CreateWebHostBuilder method returns object(that implementsIWebHostBuilder) that makes ASP.NET core app as web app Object(IWebHostBuilder).Build() method builds Webhost of this application and Run() method starts app to listen to incoming http request
+        //UseStartup(extension method) is configuring startup class(Startup.cs)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ EmployeeManagement/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        //Configures services required for the application
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure application's HTTP request processing pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+			//this env.IsDevelopment() extension method will return true only when "ASPNETCORE_ENVIRONMENT" in launchsettings.json set to development.for different environment it can be env.IsProduction() or env.IsStaging()
+            if (env.IsDevelopment())
+            {
+                //UseDeveloperExceptionPage() middleware component send response if there any exception occure in development environment.this middleware should be in the begening of pipeline
+                app.UseDeveloperExceptionPage();
+            }
+
+			//app.UseFileServer();
+
+			//Run() method is terminal middleware this Run() method takes RequestDelegate(HttpContext) handler,HttpContext object send to this delegate and using this object we can process incoming http request or outgoing response eg. context.Request.Body
+			app.Run(async (context) =>
+            {
+				await context.Response.WriteAsync("Hosting Environment :"+ env.EnvironmentName);
+                //logger.LogInformation("MW3 : Request handeled and responses produced");
+            });
+        }
+    }
+}
